e3e0e1e4520763f66ceca1826c7e0b24f4467739
syedaffanali346 <syedaffanali346@gmail.com>
2026-03-28 02:29:45 +0500

    debug an error

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,7 +1,7 @@
 import { Route, Routes } from "react-router-dom"
 import Home from "./pages/Home"
 import Blog from "./pages/Blog"
-import Layout from "./pages/Admin/Layout"
+import Layout from "./pages/admin/Layout"
 import Dashboard from "./pages/admin/Dashboard"
 import AddBlog from "./pages/admin/AddBlog"
 import ListBlog from "./pages/admin/ListBlog"
